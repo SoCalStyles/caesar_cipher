b13a90ba23301208ecdfbefbca68155c2dfcf52e
SoCalStyles <stylesaaronk@gmail.com>
2022-10-13 23:04:04 -0700

    replace if else statements w/ ternary operators

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,26 +1,17 @@
+# converts string into number
+# wraps from z to a
+# keeps same case
+
 def caesar_cipher(string, number)
   letter_codes = string.split("").map do |letter|
     x = 0
     new_letter = letter.downcase.ord
     while x < number do
-      if (new_letter == 122)
-        new_letter = 96 
-      end
-      new_letter += 1
-      if (new_letter > 122 || new_letter < 97)
-        new_letter -= 1 
-      end
+      new_letter == 122 ? new_letter = 97 : new_letter += 1
+      (new_letter > 122 || new_letter < 97) ? new_letter -= 1 : new_letter
       x += 1
     end
-    if (letter == letter.upcase)
-      new_letter = new_letter.chr.upcase
-    else
-      new_letter = new_letter.chr
-    end
+    (letter == letter.upcase) ? new_letter = new_letter.chr.upcase : new_letter = new_letter.chr
   end
   return letter_codes.join("")
 end
-
-# converts string into number
-# wraps from z to a
-# keeps same case
